3afe0dcbd5a57cbe0923b8ba615dae7a95568088
tzelepatos <62094430+tzelepatos@users.noreply.github.com>
2023-01-31 11:47:02 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,14 @@ import org.database.Database;
 public class Main {
     public static void main(String[] args) {
         mealsAppGui start = new mealsAppGui();
+        mealAppApi mealAppApi = new mealAppApi();
         start.JFrameMain();
         Database db = new Database();
         db.maladies();
+        
+        //mealAppApi.getCategoryMeal();
+        //mealAppApi.searchByName("Chicken");
+        //mealAppApi.getMealsByCategory("Seafood");
+        
     }
 }
